3af767945190e96c9bb34408a9d7e075c5b47a41
Rickard Fors <rickard.frs@gmail.com>
2018-11-03 13:21:38 +0100

    Roles and restricting views

--- TeleTime2.0/Controllers/AccountController.cs
+++ TeleTime2.0/Controllers/AccountController.cs
@@ -151,6 +151,8 @@ namespace TeleTime.Controllers
         {
             if (ModelState.IsValid)
             {
+                
+        
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
